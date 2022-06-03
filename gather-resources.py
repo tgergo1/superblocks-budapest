e40d36d1ce58d98f88c89e26216e5eb1bd76a131
Gergely Várhelyi-Tóth <gergely.toth@robogaze.com>
2022-06-03 02:06:14 +0200

    First driving map of Budapest

--- gather-resources.py
+++ gather-resources.py
@@ -1,4 +1,18 @@
+from pyrosm import OSM
 from pyrosm import get_data
 
-fp = get_data("Budapest")
-print(fp)
+budapest_data = get_data("Budapest", directory="res")
+print("Data was downloaded to:", budapest_data)
+
+osm = OSM(budapest_data)
+print("Type of 'osm' instance: ", type(osm))
+
+nodes, edges = osm.get_network(nodes=True, network_type="driving")
+
+figsizeside = 150
+
+ax = edges.plot(figsize=(figsizeside,figsizeside), color="gray")
+ax = nodes.plot(ax=ax, color="red", markersize=2.5)
+
+fig = ax.get_figure()
+fig.savefig("output.png")
