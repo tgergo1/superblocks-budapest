bae8b646142bd4263c70a436c347264ea0761083
Várhelyi-Tóth Gergely <gergely.toth@robogaze.com>
2023-02-16 16:30:03 +0100

    First capacity map

--- src/experiments/capacity_test.py
+++ src/experiments/capacity_test.py
@@ -28,23 +28,31 @@ edges["capacity"] = edges["capacity"].round(1)
 # Create a sorted list of all unique capacity values
 capacity_values = edges["capacity"].sort_values(ascending=False)
 
+top_n = 10
+
 # Create a set of the top 10 capacity values
-most_common_capacity_keys = dict(Counter(capacity_values).most_common(10)).keys()
-most_common_capacity_values = dict(Counter(capacity_values).most_common(10)).values()
+most_common_capacity_keys = dict(Counter(capacity_values).most_common(top_n)).keys()
+most_common_capacity_values = dict(Counter(capacity_values).most_common(top_n)).values()
+most_common_capacity_keys = sorted(most_common_capacity_keys, reverse=True)
 
 # Create a new column in the edges dataframe called "color" that is initially set to black
 edges["color"] = "black"
 
-# Set the color of the edges with capacity in the top 10 to red
-edges.loc[edges["capacity"].isin(most_common_capacity_keys), "color"] = "red"
+for idx, edge in edges.iterrows():
+    if edge["capacity"] in most_common_capacity_keys:
+        capacity_index = most_common_capacity_keys.index(edge["capacity"])
+        red_value = 255*(capacity_index/top_n)
+        green_value = 255-red_value
+        #edge["color"] = '#{:02X}{:02X}{:02X}'.format(round(red_value),round(green_value),0)
+        edges.at[idx, "color"] = '#{:02X}{:02X}{:02X}'.format(round(red_value),round(green_value),0)
 
 # Define the size of the figure
-figsizeside = 300
+figsizeside = 400
 
 # Plot the road network, coloring the roads based on their capacity
 fig, ax = plt.subplots(figsize=(figsizeside,figsizeside))
 edges.plot(ax=ax, color=edges["color"])
-#nodes.plot(ax=ax, color="red", markersize=1)
 
 # Save the plot to an image file
+fig.savefig("edges.svg", format="svg")
 fig.savefig("output.png")
